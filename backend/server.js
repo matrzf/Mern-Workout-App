35590d7334e02363d616e4f35651b12bbef6ddec
matrzf <matio.liantsoa@gmail.com>
2024-07-17 22:29:42 +0300

    -

--- backend/server.js
+++ backend/server.js
@@ -11,25 +11,10 @@ const userRoutes = require('./routes/userRoute')
 //Express app
 const app = express();
 
-// List of allowed origins
-const allowedOrigins = [
-  'https://mern-workout-app-frontend-fens.onrender.com',
-  'localhost:3000' // Add any additional origins here
-];
-
-// Custom CORS configuration
-const corsOptionsDelegate = (req, callback) => {
-  let corsOptions;
-  if (allowedOrigins.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false }; // disable CORS for this request
-  }
-  callback(null, corsOptions); // callback expects two parameters: error and options
-};
-
 // Use the CORS middleware with the custom options
-app.use(cors(corsOptionsDelegate));
+app.use(cors({
+  origin: 'https://mern-workout-app-frontend-fens.onrender.com' // Replace with your frontend's URL
+}));
 
 //Middleware
 app.use(express.json())
